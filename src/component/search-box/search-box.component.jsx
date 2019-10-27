54430bad2c71e73d5d4e092a1394489f378e4308
APPLE <apple@APPLEs-MacBook-Pro.local>
2019-10-27 10:50:22 +0530

    Deploy

--- src/component/search-box/search-box.component.jsx
+++ src/component/search-box/search-box.component.jsx
@@ -4,7 +4,6 @@ import './search-box.style.css';
 
 
 export const SearchBox =({handleChange, placeHolder}) => {
-    const searchEvents = (event) => this.setState({ searchText:event.target.value });
     return <input onChange={handleChange} type="search" placeholder={placeHolder}></input>;
 }
 
