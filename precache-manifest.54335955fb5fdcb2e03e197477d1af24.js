4688793a19c61f855bdc5cee45649cef2dd53616
APPLE <apple@APPLEs-MacBook-Pro.local>
2019-10-27 10:48:52 +0530

    Updates

---
+++ precache-manifest.54335955fb5fdcb2e03e197477d1af24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5eb20f0143aa4c721d934f98cb2ddad",
+    "url": "/monstorrodolex/index.html"
+  },
+  {
+    "revision": "74cef38168bcf08ece14",
+    "url": "/monstorrodolex/static/css/main.ed02ab69.chunk.css"
+  },
+  {
+    "revision": "758a4a00c2d4019be13d",
+    "url": "/monstorrodolex/static/js/2.573c64f7.chunk.js"
+  },
+  {
+    "revision": "74cef38168bcf08ece14",
+    "url": "/monstorrodolex/static/js/main.c5d92961.chunk.js"
+  },
+  {
+    "revision": "06f4a56e71a26981d11a",
+    "url": "/monstorrodolex/static/js/runtime-main.6252225e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monstorrodolex/static/media/logo.25bf045c.svg"
+  }
+]);
